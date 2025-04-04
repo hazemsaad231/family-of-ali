443aab6da0d832faf378d900a718e3fbafaa1afb
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-04-04 18:41:51 +0200

    go

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Home from './components/home'
+import Navbar from './components/navbar'
+
+function App() {
+
+  return (
+    <>
+     <Navbar/>
+     <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar.tsx
@@ -0,0 +1,26 @@
+import logo from "../assets/img/logo.png"
+
+const Navbar = () => {
+
+
+    return (
+        <div className="w-full h-max bg-gradient-to-r from-white to-orange-100">
+
+            <div className="flex items-center justify-between px-20 py-2">
+
+            <img src={logo} alt="" className="w-20 h-20" />
+
+            <h1 className="text-2xl text-right text-gray-600" style={{fontFamily: "'Amiri', serif"}}>
+                اسره محمد علي 
+            </h1>
+
+
+            </div>
+
+           
+
+        </div>
+    )
+}
+
+export default Navbar

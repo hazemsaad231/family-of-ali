49dfbb9b620962b50da90655a56e62c70c13f6e4
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-04-09 21:22:16 +0200

    done

--- src/components/Home/part1.tsx
+++ src/components/Home/part1.tsx
@@ -4,20 +4,28 @@ import image from "../../assets/img/castle.png";
   
 const One = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center px-6 py-12 text-center">
-      {/* Hero Title */}
-      <h1 className="title">
+    <div className="flex flex-col items-center justify-center my-8">
+  
+      <div>
+  {/* Hero Title */}
+  <h1 className="title">
         أسرة محمد علي
       </h1>
 
       {/* Subtitle */}
-      <p className="text-lg md:text-xl text-gray-700 max-w-2xl mb-8 animate-fade-in-up">
+      <p className="text-lg md:text-xl text-gray-700 max-w-2xl text-center p-2 ">
       رحلة عبر العصور لاكتشاف قصة الأسرة التي أسست مصر الحديثة وساهمت في نهضة الدولة اقتصاديًا واجتماعيًا، حيث أعادت هيكلة التعليم، وأطلقت مشاريع صناعية ضخمة، واهتمت بتطوير الجيش ليكون قوة فاعلة على الساحة الدولية
 
       </p>
-
+{/* Image */}
+<img
+        src={image}
+        alt="أسرة محمد علي"
+        className="w-80 sm:w-80 md:w-96 lg:w-96 xl:w-96 rounded-2xl shadow-2xl opacity-70"
+        id="image"
+      />
       {/* CTA Buttons */}
-      <div className="flex flex-col md:flex-row gap-4 mb-10 animate-fade-in-up">
+      <div className="flex flex-col justify-center items-center md:flex-row gap-4 my-8">
         <button className="bg-gray-800 hover:bg-gray-900 text-white font-medium py-3 px-6 rounded-xl shadow-lg transition">
         <a href="#home">
         ابرز الشخصيات
@@ -29,14 +37,10 @@ const One = () => {
           </a>
         </button>
       </div>
+      </div>
+    
 
-      {/* Image */}
-      <img
-        src={image}
-        alt="أسرة محمد علي"
-        className="w-80 sm:w-80 md:w-96 lg:w-[400px] xl:w-[400px] rounded-2xl shadow-2xl opacity-70"
-        id="image"
-      />
+    
     </div>
   );
 };

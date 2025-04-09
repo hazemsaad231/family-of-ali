08e565330e7ef5846beadea15e9d6798fa7b9c0c
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-04-09 15:49:54 +0200

    good

--- src/components/details.tsx
+++ src/components/details.tsx
@@ -14,12 +14,12 @@ const Details = () => {
 
 
         <div> 
-    <h2 className="text-2xl font-bold text-right mx-20 my-8 text-indigo-950">{person.name}</h2>
- <div className="bg-white w-full sm:w-full md:w-3/4 lg:w-3/4 xl:w-1/2 m-auto flex items-center flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row">
- <img src={person.img} alt={person.name} className="w-[100%] sm:w-[100%] md:w-80 lg:w-80 xl:w-80 h-80 rounded-md" />
+    <h2 className="text-2xl font-bold text-center text-indigo-950 py-6">{person.name}</h2>
+ <div className="bg-white m-auto w-full sm:w-full md:w-3/4 lg:w-3/4 xl:w-1/2 flex items-center flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row">
+ <img src={person.img} alt={person.name} className="w-[100%] sm:w-[100%] md:w-80 lg:w-80 xl:w-80 h-[20rem] sm:h-[25rem] md:h-[30rem] lg:h-[30rem] xl:h-[30rem] rounded-md" />
 
 <div className="text-right text-indigo-950 text-sm">
-      <div className="w-[100%] sm:w-[100%] md:w-[100%] lg:w-96 xl:w-96 h-80 overflow-y-auto custom-scrollbar p-8 pb-8">
+      <div className="w-[100%] sm:w-[100%] md:w-[100%] lg:w-[100%] xl:w-[100%] h-[28rem] overflow-y-auto custom-scrollbar p-4 pb-8">
         <p>{person.details}</p>
         <h3 className="font-semibold mt-1">اهم الانجازات</h3>
         <ul className="list-none">

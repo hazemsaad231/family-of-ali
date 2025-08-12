2dd37ece7fbb1d9d28bdb2f55d6a4075d28a307c
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-08-12 21:11:57 +0300

    next

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import logo from "../../assets/img/logo.png";
-import { Link } from 'react-router-dom';
 import { HashLink} from 'react-router-hash-link';
 
 

c1b03dfa83344818b6739ee5f29eca960f6f3b01
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-08-13 21:32:29 +0300

    OK

--- src/components/Outlet/master.tsx
+++ src/components/Outlet/master.tsx
@@ -1,17 +1,29 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "../Navbar/navbar"
-import Footer from "../Footer/Footer"
+import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar/navbar";
+import Footer from "../Footer/Footer";
 
 const Master = () => {
-    return (
-        <>
-            <Navbar />
-            <main>
-                    <Outlet/>
-            </main>
-            <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      {/* Background ثابت خلف كل المحتوى */}
+      <div className="fixed inset-0 z-0">
+        <img
+          src="/src/assets/img/innerBackground.jpg"
+          alt="background"
+          className="w-full h-full object-cover"
+        />
+      </div>
 
-export default Master
+      {/* المحتوى */}
+      <div className="relative z-10">
+        <Navbar />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Master;

a40b0f75ed0db8ef33c0a004c40eab17586235e7
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-08-13 21:38:10 +0300

    good

--- src/components/Outlet/master.tsx
+++ src/components/Outlet/master.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/navbar";
 import Footer from "../Footer/Footer";
+import bg from "../../assets/img/innerBg.jpg";
 
 const Master = () => {
   return (
@@ -8,7 +9,7 @@ const Master = () => {
       {/* Background ثابت خلف كل المحتوى */}
       <div className="fixed inset-0 z-0">
         <img
-          src="/src/assets/img/innerBackground.jpg"
+          src={bg}
           alt="background"
           className="w-full h-full object-cover"
         />
